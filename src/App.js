a902d1d1a30aa90366577e3f1edeae1dea701802
Iashkina Adelina <yadelishka@gmail.com>
2025-06-17 11:33:58 +0300

    slice checkWin to subfunctions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,126 @@ const cellViews = {
   2: "O",
 };
 
+function checkHorizontal(lastMoveIndex, cells, value, rowIndex, columnCount) {
+  const winningCells = [lastMoveIndex];
+
+  // Проверка вправо
+  let tempIndex = lastMoveIndex + 1;
+  while (
+    rowIndex === Math.floor(tempIndex / columnCount) &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex++;
+  }
+
+  // Проверка влево
+  tempIndex = lastMoveIndex - 1;
+  while (
+    rowIndex === Math.floor(tempIndex / columnCount) &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex--;
+  }
+
+  return winningCells;
+}
+
+function checkVertical(lastMoveIndex, cells, value, columnIndex, columnCount) {
+  const winningCells = [lastMoveIndex];
+
+  // Проверка вверх
+  let tempIndex = lastMoveIndex - columnCount;
+  while (
+    tempIndex >= 0 &&
+    columnIndex === tempIndex % columnCount &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex -= columnCount;
+  }
+
+  // Проверка вниз
+  tempIndex = lastMoveIndex + columnCount;
+  while (
+    tempIndex < cells.length &&
+    columnIndex === tempIndex % columnCount &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex += columnCount;
+  }
+
+  return winningCells;
+}
+
+function checkDiagonalLeft(lastMoveIndex, cells, value, rowIndex, columnCount) {
+  const winningCells = [lastMoveIndex];
+
+  // Проверка вниз-вправо
+  let tempIndex = lastMoveIndex + columnCount + 1;
+  let tempRow = rowIndex + 1;
+  while (
+    tempRow === Math.floor(tempIndex / columnCount) &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex += columnCount + 1;
+    tempRow++;
+  }
+
+  // Проверка вверх-влево
+  tempIndex = lastMoveIndex - columnCount - 1;
+  tempRow = rowIndex - 1;
+  while (
+    tempRow === Math.floor(tempIndex / columnCount) &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex -= columnCount + 1;
+    tempRow--;
+  }
+
+  return winningCells;
+}
+
+function checkDiagonalRight(
+  lastMoveIndex,
+  cells,
+  value,
+  rowIndex,
+  columnCount
+) {
+  const winningCells = [lastMoveIndex];
+
+  // Проверка вниз-влево
+  let tempIndex = lastMoveIndex + columnCount - 1;
+  let tempRow = rowIndex + 1;
+  while (
+    tempRow === Math.floor(tempIndex / columnCount) &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex += columnCount - 1;
+    tempRow++;
+  }
+
+  // Проверка вверх-вправо
+  tempIndex = lastMoveIndex - columnCount + 1;
+  tempRow = rowIndex - 1;
+  while (
+    tempRow === Math.floor(tempIndex / columnCount) &&
+    cells[tempIndex] === value
+  ) {
+    winningCells.push(tempIndex);
+    tempIndex -= columnCount - 1;
+    tempRow--;
+  }
+
+  return winningCells;
+}
+
 function checkWinCondition(
   lastMoveIndex,
   cells,
@@ -32,119 +152,50 @@ function checkWinCondition(
     return acc;
   }, []);
   console.log(rows, columns); */
+
   const value = cells[lastMoveIndex];
   const rowIndex = Math.floor(lastMoveIndex / columnCount);
   const columnIndex = lastMoveIndex % columnCount;
   let winningCells = [];
 
-  let winCounter = 1;
-  let tempIndex = lastMoveIndex + 1;
-  while (
-    // winCounter < winCount &&
-    rowIndex === Math.floor(tempIndex / columnCount) &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex++;
-  }
-  tempIndex = lastMoveIndex - 1;
-  while (
-    // winCounter < winCount &&
-    rowIndex === Math.floor(tempIndex / columnCount) &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex--;
-  }
-  winningCells.push(lastMoveIndex);
-  if (winCounter >= winCount) return winningCells;
+  winningCells = checkHorizontal(
+    lastMoveIndex,
+    cells,
+    value,
+    rowIndex,
+    columnCount
+  );
+  if (winningCells.length >= winCount) return winningCells;
 
-  winningCells.splice(0, winningCells.length);
-  winCounter = 1;
-  tempIndex = lastMoveIndex - columnCount;
-  while (
-    // winCounter < winCount &&
-    columnIndex === tempIndex % columnCount &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex -= columnCount;
-  }
-  tempIndex = lastMoveIndex + columnCount;
-  while (
-    // winCounter < winCount &&
-    columnIndex === tempIndex % columnCount &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex += columnCount;
-  }
-  winningCells.push(lastMoveIndex);
-  if (winCounter >= winCount) return winningCells;
+  winningCells = checkVertical(
+    lastMoveIndex,
+    cells,
+    value,
+    columnIndex,
+    columnCount
+  );
+  if (winningCells.length >= winCount) return winningCells;
 
-  winningCells.splice(0, winningCells.length);
-  winCounter = 1;
-  tempIndex = lastMoveIndex + columnCount + 1;
-  let tempRow = rowIndex + 1;
-  while (
-    // winCounter < winCount &&
-    tempRow === Math.floor(tempIndex / columnCount) &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex += columnCount + 1;
-    tempRow++;
-  }
-  tempIndex = lastMoveIndex - columnCount - 1;
-  tempRow = rowIndex - 1;
-  while (
-    // winCounter < winCount &&
-    tempRow === Math.floor(tempIndex / columnCount) &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex -= columnCount + 1;
-    tempRow--;
-  }
-  winningCells.push(lastMoveIndex);
-  if (winCounter >= winCount) return winningCells;
+  // Проверка диагонали (слева сверху - вправо вниз)
+  winningCells = checkDiagonalLeft(
+    lastMoveIndex,
+    cells,
+    value,
+    rowIndex,
+    columnCount
+  );
+  if (winningCells.length >= winCount) return winningCells;
 
-  winningCells.splice(0, winningCells.length);
-  winCounter = 1;
-  tempIndex = lastMoveIndex + columnCount - 1;
-  tempRow = rowIndex + 1;
-  while (
-    // winCounter < winCount &&
-    tempRow === Math.floor(tempIndex / columnCount) &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex += columnCount - 1;
-    tempRow++;
-  }
-  tempIndex = lastMoveIndex - columnCount + 1;
-  tempRow = rowIndex - 1;
-  while (
-    // winCounter < winCount &&
-    tempRow === Math.floor(tempIndex / columnCount) &&
-    cells[tempIndex] === value
-  ) {
-    winningCells.push(tempIndex);
-    winCounter++;
-    tempIndex -= columnCount - 1;
-    tempRow--;
-  }
-  winningCells.push(lastMoveIndex);
-  if (winCounter >= winCount) return winningCells;
+  // Проверка диагонали (справа сверху - влево вниз)
+  winningCells = checkDiagonalRight(
+    lastMoveIndex,
+    cells,
+    value,
+    rowIndex,
+    columnCount
+  );
+  if (winningCells.length >= winCount) return winningCells;
 
-  winningCells.splice(0, winningCells.length);
   return false;
 }
 
@@ -221,6 +272,7 @@ function GamePage({
   function restartGame() {
     setCells(cells.map((cell) => 0));
     setActivePlayerValue(1);
+    setWinningCells([]);
     onStartNewGame();
   }
 
